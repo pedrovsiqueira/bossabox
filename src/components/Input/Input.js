c5aaf6c658011677aeeb37ef0e983067f26b4c22
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-06-16 23:17:16 -0300

    feat: update input component to handle form

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,18 +1,53 @@
-export const Input = ({ label, type, name, placeholder, state, setState }) => {
-  return (
-    <div className="input__container">
-      <label className="input__container__label">{label}</label>
-      <input
-        type={type}
-        name={name}
-        id={name}
-        className={`input__container__input`}
-        placeholder={placeholder || ''}
-        value={state || ''}
-        onChange={event => setState(event.target.value)}
-        style={state === '' ? { border: '1px solid red' } : null}
-        required
-      />
+import { forwardRef } from 'react';
+import ReactTooltip from 'react-tooltip';
+
+import warningImg from '../../assets/warning.svg';
+
+export const Input = forwardRef(
+  ({ label, type, name, placeholder, value, onChange, onBlur, error, maxLength }, ref) => (
+    <div className={`field ${error ? 'field__input-container--error' : ''}`}>
+      <label className="field__label">{label}</label>
+      {type === 'text' && (
+        <>
+          <input
+            maxLength={maxLength}
+            type={type}
+            name={name}
+            id={name}
+            className={`field__input ${error ? 'field__input--error' : ''}`}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            onBlur={onBlur}
+            ref={ref}
+          />
+        </>
+      )}
+      {type === 'textarea' && (
+        <textarea
+          maxLength={maxLength}
+          name={name}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          placeholder={placeholder}
+          ref={ref}
+          className={`field__textarea ${error ? 'field__textarea--error' : ''}`}
+        />
+      )}
+
+      {error && (
+        <>
+          <img className="field__error-icon" data-tip={error} src={warningImg} alt="Warning" />
+          <ReactTooltip
+            textColor="#fff"
+            backgroundColor="#f95e5a"
+            type="error"
+            effect="solid"
+            place="top"
+          />
+        </>
+      )}
     </div>
-  );
-};
+  )
+);
