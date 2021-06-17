69024011273e47fc9ff89ac3b7ec0f60f67c3802
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-06-16 23:15:25 -0300

    fix: update button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,10 @@
-export const Button = ({ children, submit, onClick, color, icon }) => {
-  return (
-    <button
-      type={submit ? 'submit' : 'button'}
-      className={`btn btn--${color}`}
-      onClick={onClick || null}
-    >
-      {children}
-    </button>
-  );
-};
+export const Button = ({ children, type, onClick, color, icon, disabled }) => (
+  <button
+    disabled={disabled}
+    type={type || 'button'}
+    className={`btn btn--${color}`}
+    onClick={onClick || null}
+  >
+    {children}
+  </button>
+);
