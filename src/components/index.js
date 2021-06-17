004c80ad5e380b15ad6d1d67704a9555336b67c8
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-06-16 23:17:27 -0300

    fix: add missing components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,7 @@
 export { Button } from './Button/Button';
+export { ButtonIcon } from './Button/ButtonIcon';
 export { Input } from './Input/Input';
+export { InputSearch } from './Input/InputSearch';
+export { Form } from './Form/Form';
+export { Modal } from './Modal/Modal';
+export { ToolCard } from './ToolCard/ToolCard';
