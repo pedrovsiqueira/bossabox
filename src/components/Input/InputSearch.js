311d4d786df6479ea76b766599853d6856b16bf6
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-06-18 11:00:40 -0300

    fix: import svg as icon

--- src/components/Input/InputSearch.js
+++ src/components/Input/InputSearch.js
@@ -1,8 +1,8 @@
-import searchImg from '../../assets/search.svg';
+import { ReactComponent as SearchIcon } from '../../assets/search.svg';
 
 export const InputSearch = ({ type, name, placeholder, onChange }) => (
   <div className={`search-field`}>
-    <img className="search-field-icon" src={searchImg} alt="Magnifying glass" />
+    <SearchIcon className="search-field-icon" />
     <input
       type={type}
       name={name}
