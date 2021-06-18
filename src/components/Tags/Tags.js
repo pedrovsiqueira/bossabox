8cc3477a15464bb315e37c69463dd2869fc7dd11
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-06-18 11:00:55 -0300

    fix: remove close button

--- src/components/Tags/Tags.js
+++ src/components/Tags/Tags.js
@@ -1,28 +1,11 @@
-import { ButtonIcon } from '../';
-import closeImg from '../../assets/close.svg';
-
-export const Tags = () => {
-  const tags = ['pedro', 'joão', 'maria'];
-
-  const handleCloseTag = tag => {
-    console.log(tag);
-  };
-
-  return (
-    <div className="tags">
-      <div className="tags__container">
-        {tags.map(tag => (
-          <div key={tag} className="tag">
-            <span>#{tag}</span>
-            <ButtonIcon
-              onClick={() => handleCloseTag(tag)}
-              image={closeImg}
-              altText="Close Tag"
-              className="btn--close-tag"
-            />
-          </div>
-        ))}
-      </div>
+export const Tags = ({ tags }) => (
+  <div className="tags">
+    <div className="tags__container">
+      {tags.map((tag, index) => (
+        <div key={index} className="tag">
+          <span>#{tag}</span>
+        </div>
+      ))}
     </div>
-  );
-};
+  </div>
+);
