099f2d7087ae1172c7feb2eb55945eb04bde93ce
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-06-17 23:23:24 -0300

    feat: add toast and context providers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ContextProvider } from './hooks/toolsContext';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ContextProvider>
+    <React.StrictMode>
+      <App />
+      <ToastContainer />
+    </React.StrictMode>
+  </ContextProvider>,
   document.getElementById('root')
 );
